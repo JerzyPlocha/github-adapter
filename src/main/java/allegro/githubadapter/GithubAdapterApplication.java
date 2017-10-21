b6e57c0df3eb25b4432a9d4a15fdc6b65dc45838
Cl3aR <jerzy.plocha@gmail.com>
2017-10-21 09:42:50 +0200

    The first inital commit, source code, pom.xml
    Project built with Spring Initializer. Spring starters:
    
    * Web
    * Cache
    * Lombok

---
+++ src/main/java/allegro/githubadapter/GithubAdapterApplication.java
@@ -0,0 +1,12 @@
+package allegro.githubadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAdapterApplication.class, args);
+	}
+}
