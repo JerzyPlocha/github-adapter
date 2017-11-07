f110a0438599c764380061cb59567e80914b5597
Cl3aR <jerzy.plocha@gmail.com>
2017-11-07 20:58:47 +0100

    Code review updated deserialization test restructured

--- src/test/java/allegro/githubadapter/domain/RepositoryInformationDeserializationTest.java
+++ src/test/java/allegro/githubadapter/domain/RepositoryInformationDeserializationTest.java
@@ -8,32 +8,30 @@ import org.junit.Test;
 
 public class RepositoryInformationDeserializationTest {
 
+
     @Test
     public void shouldMapJSvariablesToJavaVariableNotation() throws Exception {
 
-        // given, when
+        // given
+        String JSON = "{\n" +
+                "  \"name\" : \"name1\",\n" +
+                "  \"description\" : \"desc1\",\n" +
+                "  \"url\" : \"https://api.github.com/repos/10/10\",\n" +
+                "  \"stargazers_count\" : 900,\n" +
+                "  \"created_at\" : \"2011-03-24T22:05:14Z\"\n" +
+                "}";
+        String JSON1 = "{\n" +
+                "  \"name\" : \"name1\",\n" +
+                "  \"description\" : \"desc1\",\n" +
+                "  \"url\" : \"https://api.github.com/repos/10/10\",\n" +
+                "  \"stars\" : 900,\n" +
+                "  \"createdAt\" : \"2011-03-24T22:05:14Z\"\n" +
+                "}";
+
+        // when
         Gson gson = new GsonBuilder().create();
-
-        GitHubRepositoryInformation actual = gson.fromJson(
-                "{\n" +
-                        "  \"name\" : \"name1\",\n" +
-                        "  \"description\" : \"desc1\",\n" +
-                        "  \"url\" : \"https://api.github.com/repos/10/10\",\n" +
-                        "  \"stargazers_count\" : 900,\n" +
-                        "  \"created_at\" : \"2011-03-24T22:05:14Z\"\n" +
-                        "}"
-                , GitHubRepositoryInformation.class);
-
-        GitHubRepositoryInformation notExpected = gson.fromJson(
-                "{\n" +
-                        "  \"name\" : \"name1\",\n" +
-                        "  \"description\" : \"desc1\",\n" +
-                        "  \"url\" : \"https://api.github.com/repos/10/10\",\n" +
-                        "  \"stars\" : 900,\n" +
-                        "  \"createdAt\" : \"2011-03-24T22:05:14Z\"\n" +
-                        "}"
-                , GitHubRepositoryInformation.class);
-
+        GitHubRepositoryInformation actual = gson.fromJson( JSON , GitHubRepositoryInformation.class);
+        GitHubRepositoryInformation notExpected = gson.fromJson( JSON1 , GitHubRepositoryInformation.class);
         GitHubRepositoryInformation expected = new GitHubRepositoryInformation();
         expected.setName("name1");
         expected.setDescription("desc1");
@@ -43,6 +41,5 @@ public class RepositoryInformationDeserializationTest {
 
         // then
         Assert.assertEquals(expected, actual);
-        Assert.assertNotEquals(notExpected, actual);
     }
 }
